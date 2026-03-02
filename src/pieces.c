05a8d47efe4347dd4cb2915ffd28941b60b1a6fc
Mike Moseley <mike.moseley@tuta.io>
2026-03-01 20:06:27 -0500

    Add piece constructor/deconstructor, remove unicode

--- src/pieces.c
+++ src/pieces.c
@@ -1,13 +1,27 @@
-#pragma once
 #include "pieces.h"
+#include <stdlib.h>
 
-char *white_glyphs[6] = {"♙","♘","♗","♖","♕","♔"}; 
-char *black_glyphs[6] = {"♟","♞","♝","♜","♛","♚"};
 char piece_chars[6] = {'p','N','B','R','Q','K'};
 
-piece_t **white_pieces = {};
-piece_t **black_pieces = {};
+piece_t **white_pieces = NULL;
+piece_t **black_pieces = NULL;
 
-piece_t *new_piece(piece_kind_t kind, chess_color_t color, vec2_t *position, vec2_t **moves){
-	
+piece_t *new_piece(piece_kind_t kind, chess_color_t color, int y, int x, vec2_t **moves) {
+	piece_t *piece = calloc(1,sizeof(piece_t));
+	piece->kind = kind;
+	piece->color = color;
+	piece->y = y;
+	piece->x = x;
+	piece->moves = moves;
+	piece->symbol = piece_chars[kind];
+
+	return piece;
 }
+
+void free_piece(piece_t *piece) {
+	// Moves will exist as a defined array of possible moves
+	// for each piece... Do not free
+	free(piece);
+}
+
+
