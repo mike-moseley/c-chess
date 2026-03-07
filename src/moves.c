604f6fb5d15b122d6e85655f520e458015393da4
Mike Moseley <mike.moseley@tuta.io>
2026-03-07 09:21:14 -0500

    Add normal pawn captures for white

--- src/moves.c
+++ src/moves.c
@@ -30,32 +30,31 @@ get_closest_move_up (struct Piece *piece, int y, int x)
 
 	int i;
 	for (i = 0; i < piece->moves_count; i++)
+	{
+		move = piece->moves[i];
+		if (move.y < y)
 		{
-			move = piece->moves[i];
-			if (move.y < y)
-				{
-					int dist_y = move.y - y;
-					/*
-					// abs because we don't care about direction here
-					*/
-					int abs_dist_x = abs (move.x - x);
+			int dist_y = move.y - y;
+			/*
+			// abs because we don't care about direction here
+			*/
+			int abs_dist_x = abs (move.x - x);
 
-					if ((dist_y < closest)
-						|| ((dist_y == closest)
-							&& abs_dist_x < second_closest))
-						{
-							closest = dist_y;
-							second_closest = abs_dist_x;
-							closest_idx = i;
-						}
-				}
+			if ((dist_y < closest)
+				|| ((dist_y == closest) && abs_dist_x < second_closest))
+			{
+				closest = dist_y;
+				second_closest = abs_dist_x;
+				closest_idx = i;
+			}
 		}
+	}
 	if (closest_idx != -1)
-		{
-			closest_vec.y = piece->moves[closest_idx].y;
-			closest_vec.x = piece->moves[closest_idx].x;
-			return closest_vec;
-		}
+	{
+		closest_vec.y = piece->moves[closest_idx].y;
+		closest_vec.x = piece->moves[closest_idx].x;
+		return closest_vec;
+	}
 	def.y = y;
 	def.x = x;
 	return def;
@@ -73,29 +72,28 @@ get_closest_move_down (struct Piece *piece, int y, int x)
 
 	int i;
 	for (i = 0; i < piece->moves_count; i++)
+	{
+		move = piece->moves[i];
+		if (move.y > y)
 		{
-			move = piece->moves[i];
-			if (move.y > y)
-				{
-					int dist_y = move.y - y;
-					int abs_dist_x = abs (move.x - x);
+			int dist_y = move.y - y;
+			int abs_dist_x = abs (move.x - x);
 
-					if ((dist_y < closest)
-						|| ((dist_y == closest)
-							&& abs_dist_x < second_closest))
-						{
-							closest = dist_y;
-							second_closest = abs_dist_x;
-							closest_idx = i;
-						}
-				}
+			if ((dist_y < closest)
+				|| ((dist_y == closest) && abs_dist_x < second_closest))
+			{
+				closest = dist_y;
+				second_closest = abs_dist_x;
+				closest_idx = i;
+			}
 		}
+	}
 	if (closest_idx != -1)
-		{
-			closest_vec.y = piece->moves[closest_idx].y;
-			closest_vec.x = piece->moves[closest_idx].x;
-			return closest_vec;
-		}
+	{
+		closest_vec.y = piece->moves[closest_idx].y;
+		closest_vec.x = piece->moves[closest_idx].x;
+		return closest_vec;
+	}
 	def.y = y;
 	def.x = x;
 	return def;
@@ -113,29 +111,28 @@ get_closest_move_left (struct Piece *piece, int y, int x)
 
 	int i;
 	for (i = 0; i < piece->moves_count; i++)
+	{
+		move = piece->moves[i];
+		if (move.x < x)
 		{
-			move = piece->moves[i];
-			if (move.x < x)
-				{
-					int dist_x = move.x - x;
-					int abs_dist_y = abs (move.y - y);
+			int dist_x = move.x - x;
+			int abs_dist_y = abs (move.y - y);
 
-					if ((dist_x < closest)
-						|| ((dist_x == closest)
-							&& abs_dist_y < second_closest))
-						{
-							closest = dist_x;
-							second_closest = abs_dist_y;
-							closest_idx = i;
-						}
-				}
+			if ((dist_x < closest)
+				|| ((dist_x == closest) && abs_dist_y < second_closest))
+			{
+				closest = dist_x;
+				second_closest = abs_dist_y;
+				closest_idx = i;
+			}
 		}
+	}
 	if (closest_idx != -1)
-		{
-			closest_vec.y = piece->moves[closest_idx].y;
-			closest_vec.x = piece->moves[closest_idx].x;
-			return closest_vec;
-		}
+	{
+		closest_vec.y = piece->moves[closest_idx].y;
+		closest_vec.x = piece->moves[closest_idx].x;
+		return closest_vec;
+	}
 	def.y = y;
 	def.x = x;
 	return def;
@@ -153,29 +150,28 @@ get_closest_move_right (struct Piece *piece, int y, int x)
 
 	int i;
 	for (i = 0; i < piece->moves_count; i++)
+	{
+		move = piece->moves[i];
+		if (move.x > x)
 		{
-			move = piece->moves[i];
-			if (move.x > x)
-				{
-					int dist_x = move.x - x;
-					int abs_dist_y = abs (move.y - y);
+			int dist_x = move.x - x;
+			int abs_dist_y = abs (move.y - y);
 
-					if ((dist_x < closest)
-						|| ((dist_x == closest)
-							&& abs_dist_y < second_closest))
-						{
-							closest = dist_x;
-							second_closest = abs_dist_y;
-							closest_idx = i;
-						}
-				}
+			if ((dist_x < closest)
+				|| ((dist_x == closest) && abs_dist_y < second_closest))
+			{
+				closest = dist_x;
+				second_closest = abs_dist_y;
+				closest_idx = i;
+			}
 		}
+	}
 	if (closest_idx != -1)
-		{
-			closest_vec.y = piece->moves[closest_idx].y;
-			closest_vec.x = piece->moves[closest_idx].x;
-			return closest_vec;
-		}
+	{
+		closest_vec.y = piece->moves[closest_idx].y;
+		closest_vec.x = piece->moves[closest_idx].x;
+		return closest_vec;
+	}
 	def.y = y;
 	def.x = x;
 	return def;
@@ -196,225 +192,254 @@ compute_moves (struct Board *board, struct Piece *piece)
 	piece->moves_count = 0;
 
 	switch (piece->kind)
+	{
+	case PAWN:
+	{
+		int i;
+		int y;
+		int x;
+		int idx;
+
+		idx = coord_to_index (board, piece->y, piece->x);
+		if (piece->color == WHITE)
 		{
-		case PAWN:
+			vec2_t move1;
+			int move1_idx;
+			cell_t *move1_cell;
+			vec2_t move2;
+			int move2_idx;
+			cell_t *move2_cell;
+
+			vec2_t capture1;
+			int capture1_idx;
+			cell_t *capture1_cell;
+			vec2_t capture2;
+			int capture2_idx;
+			cell_t *capture2_cell;
+
+			move1.y = piece->y - 1;
+			move1.x = piece->x;
+			move2.y = piece->y - 2;
+			move2.x = piece->x;
+
+			capture1.y = piece->y - 1;
+			capture1.x = piece->x - 1;
+			capture2.y = piece->y - 1;
+			capture2.x = piece->x + 1;
+
+			if (check_bounds (board, move1.y, move1.x))
 			{
-				int i;
-				int y;
-				int x;
-				int idx;
+				move1_idx = coord_to_index (board, move1.y, move1.x);
+				move1_cell = board->cells[move1_idx];
+				if (move1_cell->piece == NULL)
+				{
+					result[piece->moves_count] = move1;
+					piece->moves_count++;
+					board->cells[move1_idx]->moveable = 1;
 
-				idx = coord_to_index (board, piece->y, piece->x);
-				if (piece->color == WHITE)
+					if (piece->y == 6)
 					{
-						/* Starting row logic */
-						vec2_t move1;
-						vec2_t move2;
-						int move1_idx = 0;
-						int move2_idx = 0;
-						cell_t *move_1_cell;
-						cell_t *move_2_cell;
-						move1.y = piece->y - 1;
-						move1.x = piece->x;
-						move_1_cell = board->cells[move1_idx];
-						move2.y = piece->y - 2;
-						move2.x = piece->x;
-						move_2_cell = board->cells[move2_idx];
+						if (check_bounds (board, move2.y, move2.x))
 						{
-							if (check_bounds (board, move1.y, move1.x)
-								&& move_1_cell->piece == NULL)
-								{
-									move1_idx = coord_to_index (board, move1.y,
-																move1.x);
-									result[piece->moves_count] = move1;
-									piece->moves_count++;
-									board->cells[move1_idx]->moveable = 1;
+							move2_idx
+								= coord_to_index (board, move2.y, move2.x);
+							move2_cell = board->cells[move2_idx];
+							if (move2_cell->piece == NULL)
+							{
 
-									if (piece->y == 6)
-										{
-											if (check_bounds (board, move2.y,
-															  move2.x)
-												&& move_2_cell->piece == NULL)
-												{
-													move2_idx
-														= coord_to_index (
-															board, move2.y,
-															move2.x);
-													board->cells[move2_idx]
-														->moveable
-														= 1;
-													result[piece->moves_count]
-														= move2;
-													piece->moves_count++;
-												}
-										}
-								}
+								result[piece->moves_count] = move2;
+								board->cells[move2_idx]->moveable = 1;
+								piece->moves_count++;
+							}
 						}
 					}
-				if (piece->color == BLACK)
-					{
-					}
-
-				break;
+				}
 			}
-		case KING:
+			if (check_bounds (board, capture1.y, capture1.x))
 			{
-				int i;
-				int y;
-				int x;
-				for (i = 0; i < 8; i++)
-					{
-						y = piece->y + king_queen_moves[i].y;
-						x = piece->x + king_queen_moves[i].x;
-						if (check_bounds (board, y, x))
-							{
-								int idx = coord_to_index (board, y, x);
-								piece_t *dest = board->cells[idx]->piece;
-								if ((dest == NULL)
-									|| (dest->color != piece->color))
-									{
-										result[piece->moves_count].y = y;
-										result[piece->moves_count].x = x;
-										piece->moves_count++;
-										board->cells[idx]->moveable = 1;
-									}
-							}
-					}
+				capture1_idx = coord_to_index (board, capture1.y, capture1.x);
+				capture1_cell = board->cells[capture1_idx];
+
+				if ((capture1_cell->piece != NULL) && (capture1_cell->piece->color != piece->color))
+				{
+
+					result[piece->moves_count] = capture1;
+					board->cells[capture1_idx]->moveable = 1;
+					piece->moves_count++;
+				}
 			}
-			break;
-		case KNIGHT:
+
+			if (check_bounds (board, capture2.y, capture2.x))
 			{
-				int i;
-				int y;
-				int x;
-				for (i = 0; i < 8; i++)
-					{
-						y = piece->y + knight_moves[i].y;
-						x = piece->x + knight_moves[i].x;
-						if (check_bounds (board, y, x))
-							{
-								int idx = coord_to_index (board, y, x);
-								piece_t *dest = board->cells[idx]->piece;
-								if ((dest == NULL)
-									|| (dest->color != piece->color))
-									{
-										result[piece->moves_count].y = y;
-										result[piece->moves_count].x = x;
-										piece->moves_count++;
-										board->cells[idx]->moveable = 1;
-									}
-							}
-					}
-				break;
+				capture2_idx = coord_to_index (board, capture2.y, capture2.x);
+				capture2_cell = board->cells[capture2_idx];
+				if ((capture2_cell->piece != NULL) && (capture2_cell->piece->color != piece->color))
+				{
+					result[piece->moves_count] = capture2;
+					board->cells[capture2_idx]->moveable = 1;
+					piece->moves_count++;
+				}
 			}
-		case BISHOP:
-			{
-				int y;
-				int x;
-				int i;
-				int idx;
-				int count;
-				piece_t *dest;
-				for (i = 0; i < 4; i++)
-					{
-						for (count = 1; count < 8; count++)
-							{
-								y = piece->y + bishop_moves[i].y * count;
-								x = piece->x + bishop_moves[i].x * count;
-								if (!check_bounds (board, y, x))
-									break;
-								idx = coord_to_index (board, y, x);
-								dest = board->cells[idx]->piece;
-
-								/* Blocked by same color */
-								if (dest != NULL
-									&& dest->color == piece->color)
-									break;
-								result[piece->moves_count].y = y;
-								result[piece->moves_count].x = x;
-								piece->moves_count++;
-								board->cells[idx]->moveable = 1;
-
-								/* Capture */
-								if (dest != NULL)
-									break;
-							}
-					}
-				break;
-			}
-		case ROOK:
-			{
-
-				int y;
-				int x;
-				int i;
-				int idx;
-				int count;
-				piece_t *dest;
-				for (i = 0; i < 4; i++)
-					{
-						for (count = 1; count < 8; count++)
-							{
-								y = piece->y + rook_moves[i].y * count;
-								x = piece->x + rook_moves[i].x * count;
-								if (!check_bounds (board, y, x))
-									break;
-								idx = coord_to_index (board, y, x);
-								dest = board->cells[idx]->piece;
-
-								/* Blocked by same color */
-								if (dest != NULL
-									&& dest->color == piece->color)
-									break;
-								result[piece->moves_count].y = y;
-								result[piece->moves_count].x = x;
-								piece->moves_count++;
-								board->cells[idx]->moveable = 1;
-
-								/* Capture */
-								if (dest != NULL)
-									break;
-							}
-					}
-				break;
-			}
-		case QUEEN:
-			{
-				int y;
-				int x;
-				int i;
-				int idx;
-				int count;
-				piece_t *dest;
-				for (i = 0; i < 8; i++)
-					{
-						for (count = 1; count < 8; count++)
-							{
-								y = piece->y + king_queen_moves[i].y * count;
-								x = piece->x + king_queen_moves[i].x * count;
-								if (!check_bounds (board, y, x))
-									break;
-								idx = coord_to_index (board, y, x);
-								dest = board->cells[idx]->piece;
-
-								/* Blocked by same color */
-								if (dest != NULL
-									&& dest->color == piece->color)
-									break;
-								result[piece->moves_count].y = y;
-								result[piece->moves_count].x = x;
-								piece->moves_count++;
-								board->cells[idx]->moveable = 1;
-
-								/* Capture */
-								if (dest != NULL)
-									break;
-							}
-					}
-				break;
-			}
-		default:
-			break;
 		}
+		if (piece->color == BLACK)
+		{
+		}
+
+		break;
+	}
+	case KING:
+	{
+		int i;
+		int y;
+		int x;
+		for (i = 0; i < 8; i++)
+		{
+			y = piece->y + king_queen_moves[i].y;
+			x = piece->x + king_queen_moves[i].x;
+			if (check_bounds (board, y, x))
+			{
+				int idx = coord_to_index (board, y, x);
+				piece_t *dest = board->cells[idx]->piece;
+				if ((dest == NULL) || (dest->color != piece->color))
+				{
+					result[piece->moves_count].y = y;
+					result[piece->moves_count].x = x;
+					piece->moves_count++;
+					board->cells[idx]->moveable = 1;
+				}
+			}
+		}
+	}
+	break;
+	case KNIGHT:
+	{
+		int i;
+		int y;
+		int x;
+		for (i = 0; i < 8; i++)
+		{
+			y = piece->y + knight_moves[i].y;
+			x = piece->x + knight_moves[i].x;
+			if (check_bounds (board, y, x))
+			{
+				int idx = coord_to_index (board, y, x);
+				piece_t *dest = board->cells[idx]->piece;
+				if ((dest == NULL) || (dest->color != piece->color))
+				{
+					result[piece->moves_count].y = y;
+					result[piece->moves_count].x = x;
+					piece->moves_count++;
+					board->cells[idx]->moveable = 1;
+				}
+			}
+		}
+		break;
+	}
+	case BISHOP:
+	{
+		int y;
+		int x;
+		int i;
+		int idx;
+		int count;
+		piece_t *dest;
+		for (i = 0; i < 4; i++)
+		{
+			for (count = 1; count < 8; count++)
+			{
+				y = piece->y + bishop_moves[i].y * count;
+				x = piece->x + bishop_moves[i].x * count;
+				if (!check_bounds (board, y, x))
+					break;
+				idx = coord_to_index (board, y, x);
+				dest = board->cells[idx]->piece;
+
+				/* Blocked by same color */
+				if (dest != NULL && dest->color == piece->color)
+					break;
+				result[piece->moves_count].y = y;
+				result[piece->moves_count].x = x;
+				piece->moves_count++;
+				board->cells[idx]->moveable = 1;
+
+				/* Capture */
+				if (dest != NULL)
+					break;
+			}
+		}
+		break;
+	}
+	case ROOK:
+	{
+
+		int y;
+		int x;
+		int i;
+		int idx;
+		int count;
+		piece_t *dest;
+		for (i = 0; i < 4; i++)
+		{
+			for (count = 1; count < 8; count++)
+			{
+				y = piece->y + rook_moves[i].y * count;
+				x = piece->x + rook_moves[i].x * count;
+				if (!check_bounds (board, y, x))
+					break;
+				idx = coord_to_index (board, y, x);
+				dest = board->cells[idx]->piece;
+
+				/* Blocked by same color */
+				if (dest != NULL && dest->color == piece->color)
+					break;
+				result[piece->moves_count].y = y;
+				result[piece->moves_count].x = x;
+				piece->moves_count++;
+				board->cells[idx]->moveable = 1;
+
+				/* Capture */
+				if (dest != NULL)
+					break;
+			}
+		}
+		break;
+	}
+	case QUEEN:
+	{
+		int y;
+		int x;
+		int i;
+		int idx;
+		int count;
+		piece_t *dest;
+		for (i = 0; i < 8; i++)
+		{
+			for (count = 1; count < 8; count++)
+			{
+				y = piece->y + king_queen_moves[i].y * count;
+				x = piece->x + king_queen_moves[i].x * count;
+				if (!check_bounds (board, y, x))
+					break;
+				idx = coord_to_index (board, y, x);
+				dest = board->cells[idx]->piece;
+
+				/* Blocked by same color */
+				if (dest != NULL && dest->color == piece->color)
+					break;
+				result[piece->moves_count].y = y;
+				result[piece->moves_count].x = x;
+				piece->moves_count++;
+				board->cells[idx]->moveable = 1;
+
+				/* Capture */
+				if (dest != NULL)
+					break;
+			}
+		}
+		break;
+	}
+	default:
+		break;
+	}
 	piece->moves = result;
 }
